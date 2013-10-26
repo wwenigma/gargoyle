7f3d7b3eb034eb56e09724fb95fe5d4103dee643
wwenigma <wwenigma@gmail.com>
2013-10-26 10:10:44 +0200

    Gargoyle firmware hungarian translate

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/access.js
@@ -0,0 +1,27 @@
+﻿/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for access.sh html elements
+ */
+
+accessStr.Section="Web hozzáférés";
+accessStr.WebProtocol="Webkapcsolati protokoll";
+accessStr.LocalPort="Helyi HTTP Port";
+accessStr.Local_S_Port="Helyi HTTPS Port";
+accessStr.RemoteWebAccess="Távoli Webes hozzáférés";
+accessStr.RemotePort="Távoli HTTP Port";
+accessStr.Remote_S_Port="Távoli HTTPS Port";
+accessStr.Session="Webes bejelentkezés hossza";
+accessStr.DisablePassword="Webinterfész jelszóvédelmének kikapcsolása";
+accessStr.warning="Nem ajánlott!";
+accessStr.SSHAccess="SSH hozzáférés";
+accessStr.LocalSSHPort="Helyi SSH Port";
+accessStr.EnableRemoteAccess="Távoli SSH hozzáférés engedélyezése";
+accessStr.RemoteSSHPort="Távoli SSH Port";
+accessStr.MaxRemoteTries="Max. távoli bejelentkezési próbálkozás";
+accessStr.Attempts="Próbálkozás minden 5 percben";
+accessStr.Unlimited="Végtelen próbálkozás";
+accessStr.ChangePass="Adminisztrátori jelszó változtatása";
+accessStr.NewPass="Új jelszó";
+accessStr.ConfirmPass="Jelszó megerősítése";
+
+//javascript
+accessStr.PasswordsDiffer="Az adminisztrátori jelszó nem lett megerősítve. A megadott jelszavak nem egyeznek.";
---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/backup.js
@@ -0,0 +1,16 @@
+﻿/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for backup.sh html elements
+ */
+
+bkS.CurrC="Jelenlegi beállítások mentése";
+bkS.RestC="Régi beállítások visszaállítása";
+bkS.DfltC="Alapértelmezett beállítások visszaállítása";
+bkS.SelOF="Régi konfigurációs file tallózása";
+
+//javascript
+bkS.SelCErr="HIBA: Ki kell választanod a visszaállítandó konfigurációs file-t.";
+bkS.EraseWarn="Ez törli a jelenlegi beállításokat és a kiválasztott konfigurációs fájlban lévőkre cseréli. Biztosan folytatni szeretné?";
+bkS.UpingC="Konfigurációs fájl feltöltése";
+bkS.LdOrig="Alapértelmezett konfiguráció betöltése";
+bkS.FailErr="A visszaállítás nem sikerült. Ellenőrizze, hogy a megadott fájl érvényes Gargoyle konfigurációt tartalmaz és próbálja újra.";
+bkS.PrepBack="Mentés előkészítése";
